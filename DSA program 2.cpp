99cc2b911d545603d3956ac3316c92c00cddf158
Avinash Kumar <er.avinashpanchal@gmail.com>
2022-08-18 08:18:30 +0530

    create dsa program 2

---
+++ DSA program 2.cpp
@@ -0,0 +1,14 @@
+/* 
+2] Write a program to print using cout :
+name : your_name
+age : your_age 
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	cout<<"name\t:\tAvinash Kumar\nage\t:\t30 years";
+	return 0;
+}
